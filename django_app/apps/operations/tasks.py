5755cc3d08cbce3f477ca9a40f1f05be68199321
Trent Zhou <trent.zhou@oracle.com>
2019-03-06 20:11:47 +0800

    hack crontab string

--- django_app/apps/operations/tasks.py
+++ django_app/apps/operations/tasks.py
@@ -7,13 +7,13 @@ from utils.user_plan import UserPlan
 logger = get_task_logger(__name__)
 
 
-@periodic_task(run_every=(crontab(hour='*/2')), name="deploy_today_group_plans", ignore_result=True)
+@periodic_task(run_every=(crontab(minute='0', hour='*/2')), name="deploy_today_group_plans", ignore_result=True)
 def deploy_today_group_plans():
     logger.info("Deploying today's learning plans")
     user_plan = UserPlan()
     user_plan.deploy_group_plans_for_today()
 
-@periodic_task(run_every=(crontab(hour='*/4')), name="populate_today_tasks", ignore_result=True)
+@periodic_task(run_every=(crontab(minute='0', hour='*/4')), name="populate_today_tasks", ignore_result=True)
 def populate_today_tasks():
     logger.info("Populating today's tasks")
     user_plan = UserPlan()
